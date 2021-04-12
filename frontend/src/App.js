232239170d38c1d03056cf633d3c5e69bac637e3
Tomi Lui <tomilui1221@gmail.com>
2021-04-11 23:47:28 -0700

    game board created

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Board from './components/Board'
+
+
+function App() {
+
+  return (
+    <div>
+      <Board/>
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Board.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+import './Board.css'
+
+const BOARD_SIZE = 10
+
+const Board = () => {
+
+    const [board, setBoard] = useState( createGrid(BOARD_SIZE,BOARD_SIZE) )
+
+    function createGrid(cols, rows){
+        var arr = new Array(cols)
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = new Array(rows).fill(0)
+        }
+        console.log(arr);
+        return arr
+    }
+
+
+    return (
+        <div className="board">
+
+            {board.map( (col,colIdx) => (
+                <div key={colIdx} className="row">
+                    {col.map( (row,rowIdx) => (
+                        <div key={rowIdx} className="cell">
+                            {row}
+                        </div>
+                    ))}
+                </div>
+            ))}
+
+        </div>
+    )
+}
+
+export default Board
